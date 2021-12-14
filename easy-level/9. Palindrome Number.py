247045defb5ca04123e8b0e262a6bb5fd095ad14
djumanov <diyorbekjumanov1120@gmail.com>
2021-12-14 11:40:11 +0500

    Palindrome Number was accepted

--- easy-level/9. Palindrome Number.py
+++ easy-level/9. Palindrome Number.py
@@ -1,9 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        for i in range(len(x) // 2 + 1):
-            if x[i] != x[len(x)-1-i]:
-                return False
-            else:
-                continue
-        return True
+        if x < 0 or x % 10 == 0 and x != 0:
+            return False
+        revertedNumber = 0;
+        while x > revertedNumber:
+            revertedNumber = revertedNumber * 10 + x % 10
+            x //= 10
+        return x == revertedNumber or x == revertedNumber//10
